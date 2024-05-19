f7ae92697d370705d2c8f50b01318b1944ef4fe5
zavarykin <a.zavarykin@ase-ec.ru>
2024-05-19 11:02:52 +0300

    Initial commit

---
+++ src/main/java/com/zavarykin/realmentor/RealmentorApplication.java
@@ -0,0 +1,13 @@
+package com.zavarykin.realmentor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealmentorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealmentorApplication.class, args);
+	}
+
+}
