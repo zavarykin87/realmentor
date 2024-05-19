4b9f869783a4edea784c5ca484b8df8df5a11f97
zavarykin <a.zavarykin@ase-ec.ru>
2024-05-19 13:48:20 +0300

    renamed files

--- src/main/java/com/zavarykin/realmentor/RealMentorApplication.java
+++ src/main/java/com/zavarykin/realmentor/RealMentorApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RealmentorApplication {
+public class RealMentorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RealmentorApplication.class, args);
+		SpringApplication.run(RealMentorApplication.class, args);
 	}
 
 }
