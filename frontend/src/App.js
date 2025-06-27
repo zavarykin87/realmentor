5a532a837497a6c5215f9d5985eb7d15ca40fefb
andrewzavarykin <zavarykin87@gmail.com>
2025-06-27 09:27:37 +0300

    clean style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div>
         <Routes>
           <Route path="/" element={<HomePage />} />
         </Routes>
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 
 const HomePage = () => {
   return (
-    <div className="home-page">
-      <h1 className='text-red-700'>Добро пожаловать!</h1>
+    <div>
+      <h1>Добро пожаловать!</h1>
       <p>Это домашняя страница вашего приложения.</p>
       
-      <div className="links">
-        <Link to="/about" className="btn">О нас</Link>
-        <Link to="/contact" className="btn">Контакты</Link>
+      <div>
+        <Link to="/about">О нас</Link>
+        <Link to="/contact">Контакты</Link>
       </div>
     </div>
   );
